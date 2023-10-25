dfc9f64a858ee7db578c64059177eaea4039a3b4
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2023-10-25 22:48:38 +0300

    test gui

---
+++ main.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+
+class MyGUI(QMainWindow):
+    def __init__(self):
+        super(MyGUI, self).__init__()
+        uic.loadUi("UI_test/main_window.ui", self)
+        self.show()
+
+        self.pushButton.clicked.connect(self.open)
+
+    #initial testing
+    def open(self):
+        filename = QFileDialog.getOpenFileName()
+        path = filename[0]
+        f = open(path,"r")
+        st = ""
+        for line in f.readlines():
+            st = st + line
+            st = st + "\n"
+
+        self.textBrowser.setText(st)
+def main():
+    app = QApplication([])
+    window = MyGUI()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
+
+#
