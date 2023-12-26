f7d98c7af4c583fddc25d0f816f12314ed35af10
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2023-12-26 21:55:40 +0200

    edit gui again

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ file_field.config(state="disabled")
 file_field.grid(row=0, column=0, columnspan=10, pady=10)
 
 filepath = StringVar
-
+file_opened = False
 
 def open_file():
     global filepath
@@ -41,13 +41,9 @@ def open_file():
                 file_field.delete("1.0", END)
                 file_field.insert(END, file_content)
                 file_field.config(state="disabled")
-            scan_button.config(state="normal")
-            parse_button.config(state="normal")
+
     else:
         messagebox.showerror("File Error", "Please choose a file!!")
-
-
-file_opened = False
 
 file_button = Button(file_frame, text="Open File", command=open_file, width=68, bg="#f07167")
 file_button.grid(row=1, column=0)
@@ -64,18 +60,22 @@ label1 = Label(options_frame,text="Tokens",bg="#fed9b7")
 label1.grid(row=0, column=0, columnspan=3, pady=0)
 def scan():
     global filepath
-    obj = Scanner()
-    obj.tokenize(filepath)
-    obj.export()
-    f = open("Tokens.txt", "r", encoding='utf-8').read()
-    output_text.config(state="normal")
-    output_text.delete("1.0", END)
-    output_text.insert(END, f)
-    output_text.config(state="disabled")
+
+    if not file_opened:
+        messagebox.showerror("File Error", "Please choose a file!!")
+    else:
+        obj = Scanner()
+        obj.tokenize(filepath)
+        obj.export()
+        f = open("Tokens.txt", "r", encoding='utf-8').read()
+        output_text.config(state="normal")
+        output_text.delete("1.0", END)
+        output_text.insert(END, f)
+        output_text.config(state="disabled")
 
 
 scan_button = Button(options_frame, text="SCAN", pady=10, padx=20, width=27, bg="#0081a7", fg="#90e0ef", command=scan)
-scan_button.config(state="disabled")
+
 scan_button.grid(row=7, column=0, padx=4)
 
 
@@ -83,29 +83,31 @@ def parse():
     global status
     # scanning
     global filepath
-    obj = Scanner()
-    tokens = obj.tokenize(filepath)
-    obj.export()
-    f = open("Tokens.txt", "r", encoding='utf-8').read()
-    output_text.config(state="normal")
-    output_text.delete("1.0", END)
-    output_text.insert(END, f)
-    output_text.config(state="disabled")
-    # parsing
-    pr_obj = Parser()
-    pr_obj.tokens = tokens
-    try:
-        pr_obj.program()
-    except ValueError as v:
-        messagebox.showerror("Error", "Syntax error\n"
-                                      "This code is not accepted by Tiny language")
+    if not file_opened:
+        messagebox.showerror("File Error", "Please choose a file!!")
     else:
-        Util.generate_Parse_Tree(pr_obj.Nodes)
+        obj = Scanner()
+        tokens = obj.tokenize(filepath)
+        obj.export()
+        f = open("Tokens.txt", "r", encoding='utf-8').read()
+        output_text.config(state="normal")
+        output_text.delete("1.0", END)
+        output_text.insert(END, f)
+        output_text.config(state="disabled")
+        # parsing
+        pr_obj = Parser()
+        pr_obj.tokens = tokens
+        try:
+            pr_obj.program()
+        except ValueError as v:
+            messagebox.showerror("Error", "Syntax error\n"
+                                      "This code is not accepted by Tiny language")
+        else:
+            Util.generate_Parse_Tree(pr_obj.Nodes)
 
 
 parse_button = Button(options_frame, text="PARSE", pady=10, padx=20, width=27, bg="#0081a7", fg="#90e0ef",
                       command=parse)
-parse_button.config(state="disabled")
 parse_button.grid(row=7, column=1, padx=4)
 
 root.mainloop()
